0e91614e35c1237405983a57c0c662ee52a2bba7
Sanjayas27 <assanjay5192gmail.com@example.com>
2026-03-06 19:13:47 +0530

    Just image update

--- src/pages/AllProjects.jsx
+++ src/pages/AllProjects.jsx
@@ -6,7 +6,7 @@ import growImg from "../assets/growmytherapy.png";
 import ecommerceImg from "../assets/ecommerce.png";
 import hiredoneImg from "../assets/hiredone.png";
 import metroImg from "../assets/metro.png";
-import sloozeImg from "../assets/slooze.png"; // <-- New project image
+import sloozeImg from "../assets/slooze-logo.png"; // <-- New project image
 
 export default function AllProjects() {
   const projects = [
